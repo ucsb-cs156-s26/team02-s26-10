4e8cc9c9faab836513f80e3f42e9e0f4297bc91a
Yuhang Jiang <yuhang_jiang@ucsb.edu>
2026-05-06 16:11:58 -0700

    yj - add fixture file for helpRequest

---
+++ frontend/src/fixtures/helpRequestFixtures.js
@@ -0,0 +1,40 @@
+const helpRequestFixtures = {
+    oneRequest: {
+        id: 1,
+        requesterEmail: "string",
+        teamId: "string",
+        tableOrBreakoutRoom: "string",
+        requestTime: "2026-05-06T23:05:27.266Z",
+        explanation: "string",
+        solved: true
+    },
+    threeRequests: [
+        {
+            id: 1,
+            requesterEmail: "user1@example.com",
+            teamId: "10",
+            tableOrBreakoutRoom: "string",
+            requestTime: "2026-05-06T23:05:27.266Z",
+            explanation: "string",
+            solved: true
+        },
+        {
+            id: 2,
+            requesterEmail: "user2@example.com",
+            teamId: "10",
+            tableOrBreakoutRoom: "string",
+            requestTime: "2026-05-06T23:05:27.266Z",
+            explanation: "string",
+            solved: true
+        },
+        {
+            id: 3,
+            requesterEmail: "user3@example.com",
+            teamId: "string",
+            tableOrBreakoutRoom: "string",
+            requestTime: "2026-05-06T23:05:27.266Z",
+            explanation: "string",
+            solved: true
+        },
+    ]
+};
