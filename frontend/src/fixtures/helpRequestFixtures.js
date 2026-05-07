2d96601d80e55f8fb247628c269102dc7e5ee7d9
Yuhang Jiang <yuhang_jiang@ucsb.edu>
2026-05-06 17:12:16 -0700

    yj - fix fixture format

--- frontend/src/fixtures/helpRequestFixtures.js
+++ frontend/src/fixtures/helpRequestFixtures.js
@@ -1,40 +1,42 @@
 const helpRequestFixtures = {
-    oneRequest: {
-        id: 1,
-        requesterEmail: "string",
-        teamId: "string",
-        tableOrBreakoutRoom: "string",
-        requestTime: "2026-05-06T23:05:27.266Z",
-        explanation: "string",
-        solved: true
+  oneRequest: {
+    id: 1,
+    requesterEmail: "user1@example.com",
+    teamId: "10",
+    tableOrBreakoutRoom: "table",
+    requestTime: "2022-05-06T23:05:27",
+    explanation: "no explanation",
+    solved: true,
+  },
+  threeRequests: [
+    {
+      id: 1,
+      requesterEmail: "user1@example.com",
+      teamId: "10",
+      tableOrBreakoutRoom: "table",
+      requestTime: "2022-05-06T23:05:27",
+      explanation: "no explanation",
+      solved: true,
     },
-    threeRequests: [
-        {
-            id: 1,
-            requesterEmail: "user1@example.com",
-            teamId: "10",
-            tableOrBreakoutRoom: "string",
-            requestTime: "2026-05-06T23:05:27.266Z",
-            explanation: "string",
-            solved: true
-        },
-        {
-            id: 2,
-            requesterEmail: "user2@example.com",
-            teamId: "10",
-            tableOrBreakoutRoom: "string",
-            requestTime: "2026-05-06T23:05:27.266Z",
-            explanation: "string",
-            solved: true
-        },
-        {
-            id: 3,
-            requesterEmail: "user3@example.com",
-            teamId: "string",
-            tableOrBreakoutRoom: "string",
-            requestTime: "2026-05-06T23:05:27.266Z",
-            explanation: "string",
-            solved: true
-        },
-    ]
+    {
+      id: 2,
+      requesterEmail: "user2@example.com",
+      teamId: "10",
+      tableOrBreakoutRoom: "room",
+      requestTime: "2022-05-06T23:05:27",
+      explanation: "no explanation",
+      solved: true,
+    },
+    {
+      id: 3,
+      requesterEmail: "user3@example.com",
+      teamId: "10",
+      tableOrBreakoutRoom: "room",
+      requestTime: "2022-05-06T23:05:27",
+      explanation: "no explanation",
+      solved: true,
+    },
+  ],
 };
+
+export { helpRequestFixtures };
