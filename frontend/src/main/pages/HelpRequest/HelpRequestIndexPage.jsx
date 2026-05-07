c7c76eb834c6e09e9d7093c01861a90c84c08a3a
Yuhang Jiang <yuhang_jiang@ucsb.edu>
2026-05-06 23:04:34 -0700

    yj - copied templates for the index page

--- frontend/src/main/pages/HelpRequest/HelpRequestIndexPage.jsx
+++ frontend/src/main/pages/HelpRequest/HelpRequestIndexPage.jsx
@@ -1,17 +1,46 @@
-import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+import React from "react";
+import { useBackend } from "main/utils/useBackend";
 
-export default function HelpRequestIndexPage() {
-  // Stryker disable all : placeholder for future implementation
+import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+import RestaurantTable from "main/components/Restaurants/RestaurantTable";
+import { useCurrentUser, hasRole } from "main/utils/useCurrentUser";
+import { Button } from "react-bootstrap";
+
+export default function RestaurantIndexPage() {
+  const currentUser = useCurrentUser();
+
+  const {
+    data: restaurants,
+    error: _error,
+    status: _status,
+  } = useBackend(
+    // Stryker disable next-line all : don't test internal caching of React Query
+    ["/api/restaurants/all"],
+    { method: "GET", url: "/api/restaurants/all" },
+    // Stryker disable next-line all : don't test default value of empty list
+    [],
+  );
+
+  const createButton = () => {
+    if (hasRole(currentUser, "ROLE_ADMIN")) {
+      return (
+        <Button
+          variant="primary"
+          href="/restaurants/create"
+          style={{ float: "right" }}
+        >
+          Create Restaurant
+        </Button>
+      );
+    }
+  };
+
   return (
     <BasicLayout>
       <div className="pt-2">
-        <h1>Index page not yet implemented</h1>
-        <p>
-          <a href="/HelpRequest/create">Create</a>
-        </p>
-        <p>
-          <a href="/HelpRequest/edit/1">Edit</a>
-        </p>
+        {createButton()}
+        <h1>Restaurants</h1>
+        <RestaurantTable restaurants={restaurants} currentUser={currentUser} />
       </div>
     </BasicLayout>
   );
