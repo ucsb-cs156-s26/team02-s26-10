55c5db596a90c2f1f62dc4ede9a4564c69544d7e
Kazu <kazu@ucsb.edu>
2026-05-06 17:14:45 -0700

    table plus tests

--- frontend/src/tests/components/MenuItemReviews/MenuItemReviewsTable.test.jsx
+++ frontend/src/tests/components/MenuItemReviews/MenuItemReviewsTable.test.jsx
@@ -65,126 +65,126 @@ describe("UserTable tests", () => {
     expect(deleteButton).not.toBeInTheDocument();
   });
 
-//   test("Has the expected colum headers and content for adminUser", () => {
-//     const currentUser = currentUserFixtures.adminUser;
+  test("Has the expected colum headers and content for adminUser", () => {
+    const currentUser = currentUserFixtures.adminUser;
 
-//     render(
-//       <QueryClientProvider client={queryClient}>
-//         <MemoryRouter>
-//           <UCSBDatesTable
-//             dates={ucsbDatesFixtures.threeDates}
-//             currentUser={currentUser}
-//           />
-//         </MemoryRouter>
-//       </QueryClientProvider>,
-//     );
+    render(
+      <QueryClientProvider client={queryClient}>
+        <MemoryRouter>
+          <MenuItemReviewsTable
+            reviews={menuItemReviewsFixtures.threeReviews}
+            currentUser={currentUser}
+          />
+        </MemoryRouter>
+      </QueryClientProvider>,
+    );
 
-//     const expectedHeaders = ["id", "QuarterYYYYQ", "Name", "Date"];
-//     const expectedFields = ["id", "quarterYYYYQ", "name", "localDateTime"];
-//     const testId = "UCSBDatesTable";
+    const expectedHeaders = ["id", "Item Id", "Reviewer Email", "Stars", "Date Reviewed", "Comments"];
+    const expectedFields = ["id", "itemId", "reviewerEmail", "stars", "dateReviewed", "comments"];
+    const testId = "MenuItemReviewsTable";
 
-//     expectedHeaders.forEach((headerText) => {
-//       const header = screen.getByText(headerText);
-//       expect(header).toBeInTheDocument();
-//     });
+    expectedHeaders.forEach((headerText) => {
+      const header = screen.getByText(headerText);
+      expect(header).toBeInTheDocument();
+    });
 
-//     expectedFields.forEach((field) => {
-//       const header = screen.getByTestId(`${testId}-cell-row-0-col-${field}`);
-//       expect(header).toBeInTheDocument();
-//     });
+    expectedFields.forEach((field) => {
+      const header = screen.getByTestId(`${testId}-cell-row-0-col-${field}`);
+      expect(header).toBeInTheDocument();
+    });
 
-//     expect(screen.getByTestId(`${testId}-cell-row-0-col-id`)).toHaveTextContent(
-//       "1",
-//     );
-//     expect(screen.getByTestId(`${testId}-cell-row-1-col-id`)).toHaveTextContent(
-//       "2",
-//     );
+    expect(screen.getByTestId(`${testId}-cell-row-0-col-id`)).toHaveTextContent(
+      "1",
+    );
+    expect(screen.getByTestId(`${testId}-cell-row-1-col-id`)).toHaveTextContent(
+      "2",
+    );
 
-//     const editButton = screen.getByTestId(
-//       `${testId}-cell-row-0-col-Edit-button`,
-//     );
-//     expect(editButton).toBeInTheDocument();
-//     expect(editButton).toHaveClass("btn-primary");
+    const editButton = screen.getByTestId(
+      `${testId}-cell-row-0-col-Edit-button`,
+    );
+    expect(editButton).toBeInTheDocument();
+    expect(editButton).toHaveClass("btn-primary");
 
-//     const deleteButton = screen.getByTestId(
-//       `${testId}-cell-row-0-col-Delete-button`,
-//     );
-//     expect(deleteButton).toBeInTheDocument();
-//     expect(deleteButton).toHaveClass("btn-danger");
-//   });
+    const deleteButton = screen.getByTestId(
+      `${testId}-cell-row-0-col-Delete-button`,
+    );
+    expect(deleteButton).toBeInTheDocument();
+    expect(deleteButton).toHaveClass("btn-danger");
+  });
 
-//   test("Edit button navigates to the edit page for admin user", async () => {
-//     const currentUser = currentUserFixtures.adminUser;
+  test("Edit button navigates to the edit page for admin user", async () => {
+    const currentUser = currentUserFixtures.adminUser;
 
-//     render(
-//       <QueryClientProvider client={queryClient}>
-//         <MemoryRouter>
-//           <UCSBDatesTable
-//             dates={ucsbDatesFixtures.threeDates}
-//             currentUser={currentUser}
-//           />
-//         </MemoryRouter>
-//       </QueryClientProvider>,
-//     );
+    render(
+      <QueryClientProvider client={queryClient}>
+        <MemoryRouter>
+          <MenuItemReviewsTable
+            reviews={menuItemReviewsFixtures.threeReviews}
+            currentUser={currentUser}
+          />
+        </MemoryRouter>
+      </QueryClientProvider>,
+    );
 
-//     await waitFor(() => {
-//       expect(
-//         screen.getByTestId(`UCSBDatesTable-cell-row-0-col-id`),
-//       ).toHaveTextContent("1");
-//     });
+    await waitFor(() => {
+      expect(
+        screen.getByTestId(`MenuItemReviewsTable-cell-row-0-col-id`),
+      ).toHaveTextContent("1");
+    });
 
-//     const editButton = screen.getByTestId(
-//       `UCSBDatesTable-cell-row-0-col-Edit-button`,
-//     );
-//     expect(editButton).toBeInTheDocument();
+    const editButton = screen.getByTestId(
+      `MenuItemReviewsTable-cell-row-0-col-Edit-button`,
+    );
+    expect(editButton).toBeInTheDocument();
 
-//     fireEvent.click(editButton);
+    fireEvent.click(editButton);
 
-//     await waitFor(() =>
-//       expect(mockedNavigate).toHaveBeenCalledWith("/ucsbdates/edit/1"),
-//     );
-//   });
+    await waitFor(() =>
+      expect(mockedNavigate).toHaveBeenCalledWith("/menuitemreviews/edit/1"),
+    );
+  });
 
-//   test("Delete button calls delete callback", async () => {
-//     // arrange
-//     const currentUser = currentUserFixtures.adminUser;
+  test("Delete button calls delete callback", async () => {
+    // arrange
+    const currentUser = currentUserFixtures.adminUser;
 
-//     const axiosMock = new AxiosMockAdapter(axios);
-//     axiosMock
-//       .onDelete("/api/ucsbdates")
-//       .reply(200, { message: "Date deleted" });
+    const axiosMock = new AxiosMockAdapter(axios);
+    axiosMock
+      .onDelete("/api/menuitemreviews")
+      .reply(200, { message: "Date deleted" });
 
-//     // act - render the component
-//     render(
-//       <QueryClientProvider client={queryClient}>
-//         <MemoryRouter>
-//           <UCSBDatesTable
-//             dates={ucsbDatesFixtures.threeDates}
-//             currentUser={currentUser}
-//           />
-//         </MemoryRouter>
-//       </QueryClientProvider>,
-//     );
+    // act - render the component
+    render(
+      <QueryClientProvider client={queryClient}>
+        <MemoryRouter>
+          <MenuItemReviewsTable
+            reviews={menuItemReviewsFixtures.threeReviews}
+            currentUser={currentUser}
+          />
+        </MemoryRouter>
+      </QueryClientProvider>,
+    );
 
-//     // assert - check that the expected content is rendered
+    // assert - check that the expected content is rendered
 
-//     await waitFor(() => {
-//       expect(
-//         screen.getByTestId(`UCSBDatesTable-cell-row-0-col-id`),
-//       ).toHaveTextContent("1");
-//     });
+    await waitFor(() => {
+      expect(
+        screen.getByTestId(`MenuItemReviewsTable-cell-row-0-col-id`),
+      ).toHaveTextContent("1");
+    });
 
-//     const deleteButton = screen.getByTestId(
-//       `UCSBDatesTable-cell-row-0-col-Delete-button`,
-//     );
-//     expect(deleteButton).toBeInTheDocument();
+    const deleteButton = screen.getByTestId(
+      `MenuItemReviewsTable-cell-row-0-col-Delete-button`,
+    );
+    expect(deleteButton).toBeInTheDocument();
 
-//     // act - click the delete button
-//     fireEvent.click(deleteButton);
+    // act - click the delete button
+    fireEvent.click(deleteButton);
 
-//     // assert - check that the delete endpoint was called
+    // assert - check that the delete endpoint was called
 
-//     await waitFor(() => expect(axiosMock.history.delete.length).toBe(1));
-//     expect(axiosMock.history.delete[0].params).toEqual({ id: 1 });
-//   });
+    await waitFor(() => expect(axiosMock.history.delete.length).toBe(1));
+    expect(axiosMock.history.delete[0].params).toEqual({ id: 1 });
+  });
 });
