fabd8577d5520f0082c93618508774d86510e5e7
Alex Yoon <alex04.yoon@gmail.com>
2026-05-06 21:47:17 -0700

    alex - fixtures

---
+++ frontend/src/fixtures/ucsbDiningCommonsMenuItemFixtures.js
@@ -0,0 +1,35 @@
+const ucsbDiningCommonsMenuItemFixtures = {
+  oneMenuItem: [
+    {
+      id: 1,
+      diningCommonsCode: "ortega",
+      name: "Baked Pesto Pasta with Chicken",
+      station: "Entree Specials",
+    },
+  ],
+
+  threeMenuItems: [
+    {
+      id: 2,
+      diningCommonsCode: "ortega",
+      name: "Tofu Banh Mi Sandwich (v)",
+      station: "Entree Specials",
+    },
+
+    {
+      id: 3,
+      diningCommonsCode: "portola",
+      name: "Chicken Caesar Salad",
+      station: "Greens & Grains",
+    },
+
+    {
+      id: 4,
+      diningCommonsCode: "carrillo",
+      name: "Cream of Broccoli Soup (v)",
+      station: "Soup Bar",
+    },
+  ],
+};
+
+export { ucsbDiningCommonsMenuItemFixtures };
